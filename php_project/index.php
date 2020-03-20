aba59d76980085f45422fab8c9374dc665835e15
ramjisha <57130591+ramjisha@users.noreply.github.com>
2020-03-20 09:43:20 +0530

    Update index.php

--- php_project/index.php
+++ php_project/index.php
@@ -68,6 +68,7 @@ if ($result->num_rows > 0) {
 $conn->close();
 
 ?>
+<h1>haiii</h1>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
